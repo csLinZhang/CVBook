ab0e8c4b39c6358200165941c8625d961ba96627
csLinZhang <cslinzhang@tongji.edu.cn>
2024-04-07 12:32:43 +0800

    Add files via upload

---
+++ chapter-11-bird-eye view/surround-view/surround-view/include/birdview.h
@@ -0,0 +1,198 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+#include <string>
+#include "config.h"
+
+using namespace cv;
+using namespace std;
+
+//导入从鸟瞰视图到相机成像平面的单应矩阵
+vector<cv::Mat> ReadHomography(string aFileName)
+{
+	cv::Mat mHomography;
+	vector<cv::Mat> gHomographyMatrices;
+	gHomographyMatrices.reserve(4);
+	ifstream fIn(aFileName);
+	for (int i=0;i<4;i++)
+	{
+		string s;
+		getline(fIn, s);
+		cv::Mat_<double> iMat(3 , 3);
+		for (int j=0;j<9;j++)
+		{
+			double value;
+			fIn >> value;
+			iMat.at<double>(j/3 , j%3) = value;
+		}
+		mHomography = iMat;
+		gHomographyMatrices.push_back(mHomography);
+		getline(fIn, s);			
+	}
+	fIn.close();
+	return gHomographyMatrices;
+}
+
+//针对某个相机，生成从鸟瞰视图到最终原始鱼眼图像的映射表
+//mHomography，鸟瞰视图到相机成像平面的单应矩阵
+//mK,mD，分别为该相机的内参矩阵和畸变系数
+//iImageSize, 鸟瞰环视图的分辨率
+vector<vector<cv::Point2f>> GenerateSingleMappingTable(cv::Mat mHomography , cv::Size iImageSize , cv::Mat mK , cv::Mat mD )
+{
+	vector<vector<cv::Point2f>> gMappingTable;
+	gMappingTable.reserve(iImageSize.height); //逐行扫描
+	for (int y=0; y < iImageSize.height; y++)
+	{	
+		//生成这一行的映射信息
+		vector<cv::Point2f> gSubMappingTable;
+		gSubMappingTable.reserve(iImageSize.width);
+		for (int x=0; x < iImageSize.width; x++)
+		{
+			cv::Mat mPoint = (cv::Mat_<double>(3 , 1) << x , y , 1); //鸟瞰视图这一点的齐次坐标
+			mPoint = mHomography * mPoint; //乘上单应矩阵之后，映射到相机成像平面上（去畸变）一点
+
+			mPoint = mK.inv() * mPoint; //映射到归一化成像平面坐标系之下
+			//iPoint，归一化成像平面坐标系上的普通坐标
+			cv::Point2f iPoint(mPoint.at<double>(0 , 0)/mPoint.at<double>(2 , 0) , mPoint.at<double>(1 , 0)/mPoint.at<double>(2 , 0));
+			gSubMappingTable.push_back(iPoint);			
+		}
+		//在归一化成像平面上对点进行畸变操作。注意：得到的结果是在最终成像平面上的
+		cv::fisheye::distortPoints(gSubMappingTable, gSubMappingTable, mK, mD);
+		for (auto & item : gSubMappingTable)
+		{
+			if (item.x <= 0.0)
+			{
+				item.x = 0.0;
+			}
+			else if (item.x >= (float)(iCameraImageSize.width-1))
+			{
+				item.x = (float)(iCameraImageSize.width-1);
+			}
+			if (item.y <=0.0)
+			{
+				item.y = 0.0;
+			}
+			else if (item.y >= (float)(iCameraImageSize.height-1))
+			{
+				item.y = (float)(iCameraImageSize.height-1);
+			}
+		}
+		gMappingTable.push_back(gSubMappingTable);
+	}
+	return gMappingTable;
+}
+
+//保存从鸟瞰视图到原始鱼眼图像的映射查找表
+bool SaveMappingTable(string aFileName , vector<vector<cv::Point2f>> gMappingTable)
+{
+	ofstream fOut(aFileName , fstream::out);
+	for (auto item : gMappingTable)
+	{
+		for (auto iPoint : item)
+		{
+			fOut << iPoint << endl;
+		}
+	}
+	fOut.close();
+	return true;
+}
+
+vector<vector<cv::Point2f>> LoadMappingTable(string aFileName , cv::Size iImageSize)
+{
+	ifstream fIn(aFileName , fstream::in);
+	vector<vector<cv::Point2f>> gMappingTable;
+	gMappingTable.reserve(iImageSize.height);
+	for (int y=0;y<iImageSize.height;y++)
+	{
+		vector<cv::Point2f> gSubTable;
+		gSubTable.reserve(iImageSize.width);
+		for (int x=0;x<iImageSize.width;x++)
+		{
+			cv::Point2f iPoint;
+			string s;
+			getline(fIn, s);
+			float xx , yy;
+			sscanf_s(s.data(), "[%f, %f]", &xx , &yy);
+			iPoint.x = (double)xx;
+			iPoint.y = (double)yy;
+			gSubTable.push_back(iPoint);
+		}
+		gMappingTable.push_back(gSubTable);
+	}
+	return gMappingTable;
+}
+
+//该函数基于从鸟瞰视图平面到相机成像平面的单应矩阵以及相机内参数，生成出从鸟瞰视图上一点到鱼眼图像上一点的查找映射表
+//aIntrinsicPath,存储四个鱼眼相机内参的文件
+//aHomographyPath，存储鸟瞰视图到四个相机成像平面（去畸变之后）的单应矩阵
+void GenerateMappingTable(string aIntrinsicPath , string aHomographyPath)
+{
+	//导入四个相机的内参数据
+	FileStorage fs(aIntrinsicPath, FileStorage::READ);
+	cv::Mat mFrontK , mFrontD;
+	cv::Mat mLeftK , mLeftD;
+	cv::Mat mBackK , mBackD;
+	cv::Mat mRightK , mRightD;
+
+	fs["f_intrinsic"] >> mFrontK;
+	fs["f_distortion"] >> mFrontD;
+
+	fs["l_intrinsic"] >> mLeftK;
+	fs["l_distortion"] >> mLeftD;
+
+	fs["b_intrinsic"] >> mBackK;
+	fs["b_distortion"] >> mBackD;
+
+	fs["r_intrinsic"] >> mRightK;
+	fs["r_distortion"] >> mRightD;
+	fs.release();
+	cout << "相机内参数据导入成功！" << endl;
+
+	cv::Size iBirdsEyeSize(BIRDS_EYE_HEIGHT, BIRDS_EYE_WIDTH);
+
+	//导入从鸟瞰视图到相机成像平面的单应矩阵的数据
+	cv::Mat mHomographyFront , mHomographyLeft , mHomographyBack , mHomographyRight;
+	vector<cv::Mat> gHomographyMatrices;
+	gHomographyMatrices = ReadHomography(aHomographyPath);
+	
+	mHomographyFront = gHomographyMatrices[0];
+	mHomographyLeft = gHomographyMatrices[1];
+	mHomographyBack = gHomographyMatrices[2];
+	mHomographyRight = gHomographyMatrices[3];
+	cout << "单应矩阵数据导入成功!" << endl;
+
+	vector<vector<cv::Point2f>> gMappingTableFront = GenerateSingleMappingTable(	mHomographyFront,
+																				 	iBirdsEyeSize,
+																				 	mFrontK,
+																				 	mFrontD);
+	cout << "前视相机映射表生成成功！" << endl;
+	vector<vector<cv::Point2f>> gMappingTableLeft = GenerateSingleMappingTable(	mHomographyLeft,
+																				 	iBirdsEyeSize,
+																				 	mLeftK,
+																				 	mLeftD);
+
+	cout << "左视相机映射表生成成功！" << endl;
+	vector<vector<cv::Point2f>> gMappingTableBack = GenerateSingleMappingTable(	mHomographyBack,
+																				 	iBirdsEyeSize,
+																				 	mBackK,
+																				 	mBackD);
+
+	cout << "后视相机映射表生成成功！" << endl;
+	vector<vector<cv::Point2f>> gMappingTableRight = GenerateSingleMappingTable(	mHomographyRight,
+																				 	iBirdsEyeSize,
+																				 	mRightK,
+																				 	mRightD);
+	cout << "右视相机映射表生成成功！" << endl;
+
+	SaveMappingTable(dataDir+ "front_table.txt", gMappingTableFront);
+	cout << "前视相机映射表保存成功！" << endl;
+	SaveMappingTable(dataDir + "left_table.txt", gMappingTableLeft);
+	cout << "左视相机映射表保存成功！" << endl;
+	SaveMappingTable(dataDir + "back_table.txt", gMappingTableBack);
+	cout << "后视相机映射表保存成功！" << endl;
+	SaveMappingTable(dataDir+"right_table.txt", gMappingTableRight);
+	cout << "右视相机映射表保存成功！" << endl;
+}
+
+
---
+++ chapter-11-bird-eye view/surround-view/surround-view/surround-view.cpp
@@ -0,0 +1,233 @@
+//该程序演示了如何生成用于辅助驾驶的鸟瞰环视图
+//随程序一起提供了数据文件，放在了data文件夹中，方便读者复现
+//f.avi, l.avi，b.avi和r.avi是四个时间同步的鱼眼视频文件，它们由安装在实验车辆上的前、左、后、右四个鱼眼相机拍摄得到，我们的目标是要从
+//这四个鱼眼视频中合成出鸟瞰环视图
+//homofor4cams.txt，这个文本文件存储了从鸟瞰视图平面到四个相机成像平面（去畸变之后）的单应矩阵，由于有4个相机，所以这种矩阵一共有4个
+//这四个单应矩阵的建立需要借助标定场进行人工标定，根据点对关系通过最小二乘法计算出来
+//intrinsics.xml，这个文件存储了4个相机的内参数，包括4个3*3内参矩阵和4个镜头畸变参数
+//基于从鸟瞰视图平面到相机成像平面的单应矩阵，以及相机内参数，可以生成出从鸟瞰视图上一点到鱼眼图像上一点的查找映射表，基于
+//此查找表便可以实时生成出鸟瞰环视图
+//张林，同济大学，2024年4月
+
+#include "include/birdview.h"
+using namespace std;
+
+//该函数根据从鸟瞰视图到原始鱼眼图像的四个映射表，以及原始拍摄的鱼眼视频，生成鸟瞰环视图
+void GenerateBirdView(string aFrontPathVideo , string aLeftPathVideo , string aBackPathVideo , string aRightPathVideo ,
+					  string aFrontPath , string aLeftPath , string aBackPath , string aRightPath)
+{
+	//打开前左后右四个预先拍摄好的视频文件
+	cv::VideoCapture iCaptureFront;
+	iCaptureFront.open(aFrontPathVideo);
+	cv::VideoCapture iCaptureLeft;
+	iCaptureLeft.open(aLeftPathVideo);
+	cv::VideoCapture iCaptureBack;
+	iCaptureBack.open(aBackPathVideo);
+	cv::VideoCapture iCaptureRight;
+	iCaptureRight.open(aRightPathVideo);
+
+	//鸟瞰视图分辨率
+	cv::Size sWH(BIRDS_EYE_WIDTH , BIRDS_EYE_HEIGHT);
+	//读入四个相机的映射表
+	vector<vector<cv::Point2f>> gFrontMappingTable , gLeftMappingTable , gBackMappingTable ,gRightMappingTable;
+	gFrontMappingTable = LoadMappingTable(aFrontPath , sWH);
+	gLeftMappingTable = LoadMappingTable(aLeftPath , sWH);
+	gBackMappingTable =  LoadMappingTable(aBackPath , sWH);
+	gRightMappingTable = LoadMappingTable(aRightPath , sWH);
+	cout << "完成4个映射表读取" << endl;
+	   
+	int W = BIRDS_EYE_WIDTH , H = BIRDS_EYE_HEIGHT;
+	
+	//针对四个相机，初始化四个鸟瞰视图，分辨率与最终拼合好的环视图一致
+	//要先生成四个鸟瞰视图，然后在确定好拼接线的基础上，从四个鸟瞰视图拼合成最终鸟瞰环视图
+	cv::Mat iFrontBirdsEyeFrame , iLeftBirdsEyeFrame , iBackBirdsEyeFrame , iRightBirdsEyeFrame;
+	cv::Mat iBirdsEyeImage= cv::Mat::zeros(W,H,CV_8UC3);
+	iFrontBirdsEyeFrame = cv::Mat::zeros(W,H,CV_8UC3);
+	iLeftBirdsEyeFrame = cv::Mat::zeros(W,H,CV_8UC3);
+	iBackBirdsEyeFrame = cv::Mat::zeros(W,H,CV_8UC3);
+	iRightBirdsEyeFrame = cv::Mat::zeros(W,H,CV_8UC3);
+	   
+	bool bStop = false;
+
+    //确定好输出路径，将生成的环视视频保存在本地
+	string outputVideoPath = dataDir + "surround-view-video.avi";
+    cv::VideoWriter outputVideo;
+    outputVideo.open(outputVideoPath, cv::VideoWriter::fourcc('M', 'P', '4', '2'), 25.0, sWH);
+
+	cv::Mat iBirdsEyeImageCopy = iBirdsEyeImage.clone();
+	while (!bStop)
+	{
+		//从四路视频中，读入当前帧并陆续存入gImages中
+		vector<cv::Mat> gImages;
+		cv::Mat iFrontFrame , iLeftFrame , iBackFrame , iRightFrame;
+		bool bRes1 = iCaptureFront.read(iFrontFrame);
+		bool bRes2 = iCaptureLeft.read(iLeftFrame);
+		bool bRes3 = iCaptureBack.read(iBackFrame);
+		bool bRes4 = iCaptureRight.read(iRightFrame);
+		if (!bRes1 || !bRes2 || !bRes3 || !bRes4)
+		{
+			break;
+		}
+		gImages.push_back(iFrontFrame);
+		gImages.push_back(iLeftFrame);
+		gImages.push_back(iBackFrame);
+		gImages.push_back(iRightFrame);		
+
+		//生成前鸟瞰视图
+		int maxV = (BIRDS_EYE_HEIGHT-CENTER_HEIGHT)/2;
+		int minV = 0;
+		int maxU = (BIRDS_EYE_WIDTH/4*3);
+		int minU = BIRDS_EYE_WIDTH/4;
+		for (int v=0;v<= maxV;v++)
+		{
+				for (int u= minU;u<= maxU;u++)
+				{
+                    //Front area       
+					cv::Point2f iMapping = gFrontMappingTable[v][u];
+					iFrontBirdsEyeFrame.at<cv::Vec3b>(v , u) = gImages[0].at<cv::Vec3b>((int)iMapping.y , (int) iMapping.x);
+	            }
+	    }
+
+		//生成左鸟瞰视图
+	    maxV = BIRDS_EYE_HEIGHT;
+	    maxU = BIRDS_EYE_WIDTH/2;
+	    int subMaxV = (BIRDS_EYE_HEIGHT - CENTER_HEIGHT)/2;
+	    int subMinV = (BIRDS_EYE_HEIGHT + CENTER_HEIGHT)/2;
+	    int subMaxU = (BIRDS_EYE_WIDTH - CENTER_WIDTH)/2;
+		for (int v=0;v< maxV;v++)
+		{
+				for (int u=0;u<=maxU;u++)
+				{
+	                    //Left area.
+						cv::Point2f iMapping = gLeftMappingTable[v][u];
+						iLeftBirdsEyeFrame.at<cv::Vec3b>(v , u) = gImages[1].at<cv::Vec3b>((int)iMapping.y , (int) iMapping.x);	
+	            }
+	    }
+
+		//生成后鸟瞰视图
+	    minV = (BIRDS_EYE_HEIGHT + CENTER_HEIGHT)/2;
+	    maxV = BIRDS_EYE_HEIGHT;
+	    minU = BIRDS_EYE_WIDTH/4;
+	    maxU = (BIRDS_EYE_WIDTH/4*3);
+		for (int v= minV;v< maxV;v++)
+		{
+				for (int u= minU;u<= maxU;u++)
+				{
+                    //Back area.    
+					cv::Point2f iMapping = gBackMappingTable[v][u];
+					iBackBirdsEyeFrame.at<cv::Vec3b>(v , u) = gImages[2].at<cv::Vec3b>((int)iMapping.y , (int) iMapping.x);	
+	            }
+	    }
+
+		//生成右鸟瞰视图
+	    minU = BIRDS_EYE_WIDTH/2;
+	    subMinV = (BIRDS_EYE_WIDTH + CENTER_WIDTH)/2;
+	    subMaxV = (BIRDS_EYE_HEIGHT - CENTER_HEIGHT)/2;
+	    int subMinU = (BIRDS_EYE_WIDTH + CENTER_WIDTH)/2;
+		for (int v=0;v<BIRDS_EYE_HEIGHT;v++)
+		{
+				for (int u= minU;u<BIRDS_EYE_WIDTH;u++)
+				{	
+        			//Right area.
+					cv::Point2f iMapping = gRightMappingTable[v][u];
+					iRightBirdsEyeFrame.at<cv::Vec3b>(v , u) = gImages[3].at<cv::Vec3b>((int)iMapping.y , (int) iMapping.x);
+	            }
+	    }
+
+		//填充合成鸟瞰环视图中来自前视的部分
+		maxV = (BIRDS_EYE_HEIGHT-CENTER_HEIGHT)/2;
+		minV = 0;
+		maxU = (BIRDS_EYE_WIDTH/4*3);
+		minU = BIRDS_EYE_WIDTH/4;
+		for (int v=0;v<= maxV;v++)
+		{
+				for (int u= minU;u<= maxU;u++)
+				{
+		            //Front area                                                
+						iBirdsEyeImage.at<cv::Vec3b>(v , u) = iFrontBirdsEyeFrame.at<cv::Vec3b>(v , u);
+				}
+	    }
+		//填充合成鸟瞰环视图中来自左视的部分
+		maxV = BIRDS_EYE_HEIGHT;
+	    maxU = BIRDS_EYE_WIDTH/2;
+
+	    subMaxV = (BIRDS_EYE_HEIGHT - CENTER_HEIGHT)/2;
+	    subMinV = (BIRDS_EYE_HEIGHT + CENTER_HEIGHT)/2;
+	    subMaxU = (BIRDS_EYE_WIDTH - CENTER_WIDTH)/2;
+		for (int v=0; v<maxV; v++)
+		{
+				for (int u=0; u<=subMaxU; u++)
+				{
+			        if (u<((BIRDS_EYE_WIDTH)/4+v/2) && u <= ((BIRDS_EYE_WIDTH+2*BIRDS_EYE_HEIGHT)/4-v/2))
+					{
+			                //Left area.            	
+							iBirdsEyeImage.at<cv::Vec3b>(v , u) = iLeftBirdsEyeFrame.at<cv::Vec3b>(v , u);
+			    	}
+			}
+	    }
+		//填充合成鸟瞰环视图中来自后视的部分
+ 		minV = (BIRDS_EYE_HEIGHT + CENTER_HEIGHT)/2;
+	    maxV = BIRDS_EYE_HEIGHT;
+	    minU = BIRDS_EYE_WIDTH/4;
+	    maxU = (BIRDS_EYE_WIDTH/4*3);
+		for (int v= minV;v< maxV;v++)
+		{
+				for (int u= minU;u<= maxU;u++)
+				{
+			        if (u < ((BIRDS_EYE_WIDTH)/4+v/2) && u > ((BIRDS_EYE_WIDTH+2*BIRDS_EYE_HEIGHT)/4-v/2))
+					{
+			                //Back area.    
+							iBirdsEyeImage.at<cv::Vec3b>(v , u) = iBackBirdsEyeFrame.at<cv::Vec3b>(v , u);
+			    	}
+			    }
+	    }
+		//填充合成鸟瞰环视图中来自右视的部分
+        minU = BIRDS_EYE_WIDTH/2;
+	    subMinV = (BIRDS_EYE_WIDTH + CENTER_WIDTH)/2;
+	    subMaxV = (BIRDS_EYE_HEIGHT - CENTER_HEIGHT)/2;
+	    subMinU = (BIRDS_EYE_WIDTH + CENTER_WIDTH)/2;
+		for (int v=0;v<BIRDS_EYE_HEIGHT;v++)
+		{
+			for (int u= subMinU;u<BIRDS_EYE_WIDTH;u++)
+			{	   		
+		        if (u>=((BIRDS_EYE_WIDTH)/4+v/2) && u > ((BIRDS_EYE_WIDTH+2*BIRDS_EYE_HEIGHT)/4-v/2)) 
+				{
+		            	//Right area.
+						iBirdsEyeImage.at<cv::Vec3b>(v , u) = iRightBirdsEyeFrame.at<cv::Vec3b>(v , u);	
+		        }
+	        }
+	    }
+
+		cv::imshow("show", iBirdsEyeImage);
+		outputVideo << iBirdsEyeImage;
+
+		//Clear the images.
+		gImages.clear();
+		if (cv::waitKey(10) >= 0)
+		{
+			bStop = true;		   	
+		}
+	}
+	outputVideo.release();
+	cout << "完成鸟瞰环视图保存！" << endl;
+}
+
+int main()
+{
+	//该函数基于从鸟瞰视图平面到相机成像平面的单应矩阵以及相机内参数，生成出从鸟瞰视图上一点到鱼眼图像上一点的查找映射表
+	//intrinsics.xml,存储四个鱼眼相机内参的文件
+	//homofor4cams.txt，存储鸟瞰视图到四个相机成像平面（去畸变之后）的单应矩阵
+	//查找表一共有4个，对应于4个相机，分别命名为front_table.txt，lef_table.txt,back_table.txt和right_table.txt
+	GenerateMappingTable(dataDir + "intrinsics.xml", dataDir+ "homofor4cams.txt");
+	cout << "映射表生成任务完成！" << endl;
+	
+	//根据四个视图的映射查找表和原始四路鱼眼视频，生成鸟瞰环视图
+	GenerateBirdView(dataDir+ "f.avi", dataDir+"l.avi",
+		             dataDir+"b.avi", dataDir + "r.avi",
+		             dataDir+"front_table.txt", 
+					 dataDir+"left_table.txt", 
+				     dataDir+"back_table.txt", 
+					 dataDir+"right_table.txt");
+	
+	return 0;
+}
