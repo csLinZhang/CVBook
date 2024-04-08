2a766aa28ea69e2466ced2f872fce8170b6a9c87
csLinZhang <cslinzhang@tongji.edu.cn>
2024-04-08 15:57:25 +0800

    Add files via upload

--- chapter-06-homography estimation/PanoramaStichingUsingSIFTRANSAC/homography2d.m
+++ chapter-06-homography estimation/PanoramaStichingUsingSIFTRANSAC/homography2d.m
@@ -1,6 +1,5 @@
 % 从点对集合data中按照最小二乘法估计射影矩阵，具体方法见教材5.1。
 % data的维度为6xN，每一列是一个点对，每个点的表示方式必须是规范化二维齐次坐标
-% 
 % H，3x3的射影矩阵，cx2 = H*x1
 
 function H = homography2d(data)
@@ -10,7 +9,7 @@ function H = homography2d(data)
     A = zeros(2*Npts,9); %初始化系数矩阵
     
     O = [0 0 0];
-    %下面这个循环时构造系数矩阵A
+    %此循环是构造系数矩阵A（见教材式5-7中的矩阵A）
     for i = 1:Npts
 	    point1i = points1(:,i)';
 
